fb0c648ca612c7567c7b17e643cf34fd3c476eab
Igor Kiselyov <igor.kiselyov@lgm.ca>
2017-02-24 21:47:33 -0800

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent,TopbarComponent, MenuComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+
+import {LoginComponent} from './login/login.component';
+
+export const AppRoutes = [
+  {path:'login',component: LoginComponent}
+];
